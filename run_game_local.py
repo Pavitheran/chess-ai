d38f5f2e69855d786029d6688b878e0e661b79d2
alinawed <ali.nawed@prepr.org>
2019-08-10 11:25:19 -0400

    Added chess board visual

--- run_game_local.py
+++ run_game_local.py
@@ -1,15 +1,17 @@
 from bots.random_bot import RandomBot
 
 import chess
+import chess.svg
 
 """
-Method consumes two ChessBot implementations and returns 
-"BLACK" if black wins and "WHITE" if white wins.
+Method consumes two ChessBot implementations and returns
+"BLACK" if black wins and "WHITE" if white wins. Along with the
+representation of the bord in standard chess notation.
 """
 def run_game(white, black):
 	board = chess.Board()
 	turn = 0
-	# Board.is_game_over() checks for stalemates, 
+	# Board.is_game_over() checks for stalemates,
 	# insufficient material and repetition.
 	while not board.is_checkmate() and not board.is_game_over():
 		# White plays on even turns and black plays on odd turns.
@@ -20,16 +22,15 @@ def run_game(white, black):
 			black.play_move(board)
 		turn += 1
 	else:
-		print(board)
-		# When loop terminates by loop condition, 
+		# When loop terminates by loop condition,
 		# the previous player who moved wins.
 		if board.is_checkmate():
 			if turn % 2 == 0:
-				return "BLACK ({})".format(black.get_name())
+				return "BLACK ({})".format(black.get_name()), board
 			else:
-				return "WHITE ({})".format(white.get_name())
+				return "WHITE ({})".format(white.get_name()), board
 		else:
-			return "DRAW"
+			return "DRAW", board
 
 
 if __name__ == '__main__':
@@ -37,5 +38,7 @@ if __name__ == '__main__':
 	# Replace below RandomBot instantiations with custom Bot implementations
 	white = RandomBot("Random Bot 1")
 	black = RandomBot("Random Bot 2")
-	winner = run_game(white, black)
+	winner, board = run_game(white, black)
+	#Using chess libray graphics interface to render board
+	chess.svg.board(board=board)
 	print("RESULT: " + winner)
