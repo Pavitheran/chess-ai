56260de4c7ec566b490c61946cdd095af49bdd8a
alinawed <ali.nawed@prepr.org>
2019-10-03 21:00:16 -0400

    removed accept_surrender from base bot

--- bots/base_chess_bot.py
+++ bots/base_chess_bot.py
@@ -14,11 +14,3 @@ class BaseChessBot(ABC):
 	@abstractmethod
 	def play_move(self, board):
 		pass
-
-	"""
-	Given a standard chess game representation,
-	method should return True if the bot wants to accept a surrender
-	and false otherwise.
-	"""
-	def accept_surrender(self, board):
-		return False
