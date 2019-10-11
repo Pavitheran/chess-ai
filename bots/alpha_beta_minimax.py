346b1ca12b91d6fd902b35c5fc4bdaf0e170d3e1
alinawed <ali.nawed@prepr.org>
2019-10-11 18:35:20 -0400

    refactored code to adhere to PEP8 and PEP257

--- bots/alpha_beta_minimax.py
+++ bots/alpha_beta_minimax.py
@@ -1,23 +1,34 @@
-def minimax(board, depth, eval, alpha, beta, maximizing_player):
-    '''
+"""
+Implementation of the minimax algorithm.
+
+Optimized with alpha-beta pruning.
+"""
+
+
+# pylint: disable-msg=R0913
+def minimax(board, depth, eval_func, alpha, beta, maximizing_player):
+    """
+    Minimax Algorithm implemented to work with chess.Board object.
+
     @param chess.Board board
     @param int depth
+    @param float alpha
+    @param float beta
     @param function eval #function to evaluate the board position
     @param boot maximizing_player
     @rtype: numeric, chess.Move
-    '''
+    """
     if depth == 0 or board.is_game_over():
-        return eval(board), None
+        return eval_func(board), None
 
     legal_moves = list(board.legal_moves)
-
 
     if maximizing_player:
         max_eval = -1e+10
         best_move = ''
         for move in legal_moves:
             board.push(move)
-            value, _ = minimax(board, depth-1, eval, alpha, beta, 0)
+            value, _ = minimax(board, depth-1, eval_func, alpha, beta, 0)
 
             if max_eval < value:
                 max_eval = value
@@ -25,23 +36,25 @@ def minimax(board, depth, eval, alpha, beta, maximizing_player):
 
             alpha = max(value, alpha)
             board.pop()
+
             if beta <= alpha:
                 break
         return max_eval, best_move
 
-    else:
-        min_eval = 1e+10
-        worst_move = ''
-        for move in legal_moves:
-            board.push(move)
-            value, _ = minimax(board, depth-1, eval, alpha, beta, 1)
+    min_eval = 1e+10
+    worst_move = ''
 
-            if min_eval > value:
-                min_eval = value
-                worst_move = move
+    for move in legal_moves:
+        board.push(move)
+        value, _ = minimax(board, depth-1, eval_func, alpha, beta, 1)
 
-            beta = min(value, beta)
-            board.pop()
-            if beta <= alpha:
-                break
+        if min_eval > value:
+            min_eval = value
+            worst_move = move
+
+        beta = min(value, beta)
+        board.pop()
+
+        if beta <= alpha:
+            break
         return min_eval, worst_move
--- bots/simple_bot.py
+++ bots/simple_bot.py
@@ -1,13 +1,14 @@
-from bots.base_chess_bot import BaseChessBot
-from bots.alpha_beta_minimax import minimax
-import chess
-import random
-import numpy as np
 """
-This example bot implementation uses the simple_eval measure of a Board
+This example bot implementation uses the simple_eval measure of a Board.
+
 which is the difference between the wieghted value of all the pieces on each
+
 side
 """
+
+from base_chess_bot import BaseChessBot
+from alpha_beta_minimax import minimax
+import chess
 
 CHESS_PIECES = [chess.PAWN,
                 chess.KNIGHT,
@@ -15,40 +16,56 @@ CHESS_PIECES = [chess.PAWN,
                 chess.ROOK,
                 chess.QUEEN]
 
+
 def eval_side(board, color):
-    '''
+    """
+    Evaluate board for a player position.
+
     @param chess.Board board
     @param bool color
         0 - black
         1 - white
     @rtype int
-    '''
-
-    pieces = [len(board.pieces(piece,color))*piece for piece in CHESS_PIECES]
+    """
+    pieces = [len(board.pieces(piece, color))*piece for piece in CHESS_PIECES]
 
     return sum(pieces)
 
 
 class SimpleBot(BaseChessBot):
+    """Simple bot class inherits from BaseChessBot interface."""
 
     def __init__(self, name, color):
+        """
+        Initilize SimpleBot.
 
+        @param str name
+        @param bool color
+            - black: 0
+            - white: 1
+        """
         super().__init__()
 
         self.name = name
         self.color = color
 
     def get_name(self):
+        """Return name of bot."""
         return self.name
 
     def simple_eval(self, board):
+        """
+        Return evaluation for chess board.
+
+        @param chess.Board board
+        """
         if board.is_checkmate():
             return 1e+10
-        else:
-            val = eval_side(board, 1) - eval_side(board, 0)
-            return val if self.color==1 else -1*val
+
+        val = eval_side(board, 1) - eval_side(board, 0)
+        return val if self.color == 1 else -1*val
 
     def play_move(self, board):
-
-        value, move = minimax(board, 3, self.simple_eval, -1e+10, 1e+10, 1)
+        """Push move onto the board."""
+        _, move = minimax(board, 3, self.simple_eval, -1e+10, 1e+10, 1)
         board.push(move)
